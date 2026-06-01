0ef1ace85e80fa722440cc09e8c75e1f716ce247
bnaveenniranjan <112605495+bnaveenniranjan@users.noreply.github.com>
2026-06-01 15:19:22 +0530

    (Asynchronous)

--- core-modules/asyncFs.js
+++ core-modules/asyncFs.js
@@ -1,9 +1,12 @@
 const fs =  require("fs");
 
 // Write File (Asynchronous)
-fs.writeFile("asyncTest.txt","hello Nodejs  2025 Async",(err) =>{
+fs.writeFile("asyncTest.txt","hello Nodejs  2025 Async + Read File ",(err) =>{
     if(err) throw err; 
     console.log("File written completed")
-});
 
+fs.readFile("asyncTest.txt",{encoding:"utf-8"},(err,data) => {    
+    console.log("Read  File Async ",data);
+    })
+});
 console.log("FIle writing processing")
