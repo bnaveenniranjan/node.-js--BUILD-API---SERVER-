41b22682d6f742124e8623eb087beace4672be65
bnaveenniranjan <112605495+bnaveenniranjan@users.noreply.github.com>
2026-06-02 19:46:14 +0530

    post ,get , login methods

--- express.js/index.js
+++ express.js/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res ,next)=>{
+    console.log("request URL",req.url);
+    next();  
+})
 app.get("/",(req,res) => {
     res.status(200).send("Hello Express App")
       
@@ -8,6 +12,7 @@ app.get("/",(req,res) => {
 app.post("/",(req,res) => {
     res.status(200).send("Hello Express App ! . this is post method")
 })
+
 app.listen(5000,() => {
     console.log("server listening on port 5000")
 })
